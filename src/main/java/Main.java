f9c5ef19d1d27863d710d187faf3cbc877afa78e
nette74 <spphire2@gmail.com>
2022-07-07 15:10:29 +0900

    추상화

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,22 @@ public class Main {
         //run();
         System.out.println("===명언 SSG==");
         Scanner sc = new Scanner(System.in);
+
         outer:
         while (true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
             switch (cmd) {
+                case "등록":
+
+                case "목록":
+
+                case "수정":
+
+                case "삭제":
+
                 case "종료":
+
                     break outer;
             }
         }
